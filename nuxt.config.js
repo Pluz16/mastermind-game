b09f96bbcc7734e6bb5dfcb1979aaf5ded458bea
Francesco Punta <puntafrancesco@gmail.com>
2024-10-25 22:47:54 +0200

    Solved installando otra vez dependencias

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,34 @@
 export default {
-  // Disable server-side rendering
   ssr: false,
 
-  // Global page headers
   head: {
     title: 'mastermind-game',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    htmlAttrs: { lang: 'en' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  // Global CSS (includi il file tailwind qui)
   css: [
     '@/assets/css/tailwind.css'
   ],
 
-  // Auto import components
-  components: true,
-
-  // Build and dev modules
   buildModules: [
     '@nuxt/typescript-build',
     '@nuxtjs/tailwindcss',
   ],
 
-  // Build configuration
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
   build: {},
+  compatibilityDate: '2024-10-25',
 };
